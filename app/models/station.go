5cbb7323c6af805d8f1050ae7f2c89403feea2d3
Jonathan.Komma <jonathan.komma@xpublisher.com>
2023-04-20 06:22:31 +0000

    begin update data

--- app/models/station.go
+++ app/models/station.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
 
 type Station struct {
 	url         string
@@ -11,4 +16,18 @@ type Station struct {
 
 func (s *Station) NewStation(url string) {
 	s.url = url
+}
+
+func (s *Station) UpdateData() {
+	resp, err := http.Get(s.url)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 }
